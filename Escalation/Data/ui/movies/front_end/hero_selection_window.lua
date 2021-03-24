032a406c2e42a435855b276b8b7c70d50ac6ec15
iToaster <itoasterofficial@gmail.com>
2021-03-24 00:34:01 -0700

    Company Deployment Beacon now has animation that lines up with Armory, tested some other stuff to no luck

---
+++ Escalation/Data/ui/movies/front_end/hero_selection_window.lua
@@ -0,0 +1,34 @@
+units = 
+{
+	{
+		rotation = 0;
+		randomStates =
+		{
+ 			{ "paperdoll", "on", -1.0, -1.0, 0 },
+		}
+	},
+	{
+		rotation = -30;
+		randomStates =
+		{
+ 			{ "paperdoll", "on", -1.0, -1.0, 0 },		
+ 			{ "cover", "on", 2.0, 5.0, 1 },
+			{ "cover", "off", 6.0, 7.0, 0 },
+			{ "range_weapon_state", "reload", 1.0, 3.0, 2 },
+			{ "range_weapon_state", "ready", 4.0, 6.0, 2 },		
+		}				
+	},	
+	{
+		rotation = 30;
+		randomStates =
+		{
+ 			{ "paperdoll", "on", -1.0, -1.0, 0 },		
+ 			{ "cover", "on", 2.0, 5.0, 1 },
+ 			{ "cover", "off", 6.0, 7.0, 0 },
+			{ "range_weapon_state", "reload", 1.0, 3.0, 2 },
+			{ "range_weapon_state", "ready", 4.0, 6.0, 2 },
+		}		
+	}	
+};
+weaponReadyTime = 0.0;
+weaponFireTime = 2.0;
